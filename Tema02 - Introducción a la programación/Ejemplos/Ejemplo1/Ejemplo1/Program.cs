b10d5a2867776fdec791686bf313ac161d60796a
Martín García <martin.garcia.figueira@ciclosmontecastelo.com>
2022-10-17 21:13:10 +0200

    Tema 1 y 2

---
+++ Tema02 - Introducción a la programación/Ejemplos/Ejemplo1/Ejemplo1/Program.cs
@@ -0,0 +1,18 @@
+﻿//Declaramos la variable
+int numero;
+
+//Pedimos el numero por pantalla
+Console.Write("Dame un número: ");
+numero = int.Parse(Console.ReadLine());
+
+//Comprobamos si es par o impar
+if (numero % 2 == 0)
+{
+    Console.WriteLine("ES PAR");
+}
+else
+{
+    Console.WriteLine("ES IMPAR");
+}
+
+Console.ReadLine();
