3f85af43ebbf1db08cb628a63adca67f887e30e8
Martin <martin.garcia.figueira@ciclosmontecastelo.com>
2022-11-02 14:17:33 +0100

    Ejercicio5

---
+++ Tema02 - Introducción a la programación/Ejercicios/Bloque 2/Ejercicio5/Ejercicio5/Program.cs
@@ -0,0 +1,18 @@
+﻿//Declaramos las variables
+double precio, importeFinal;
+short cantidad;
+
+//Pedimos los datos al usuario
+Console.WriteLine("Introduce la cantidad");
+cantidad = short.Parse(Console.ReadLine());
+
+Console.WriteLine("Introduce el precio");
+precio = double.Parse(Console.ReadLine());
+
+//Hacemos los cálculos
+importeFinal = precio * cantidad;
+
+//Mostramos los resultados
+Console.WriteLine("El precio del producto es " + precio + " euros y la cantidad que compra es " + cantidad + " por lo tanto debe pagar " + importeFinal + " euros. Vuelva pronto.");
+
+Console.ReadLine();
