93bdb8193ce1a9cc36d3e4f689eaef9977f69471
Martin <martin.garcia.figueira@ciclosmontecastelo.com>
2022-11-02 14:45:48 +0100

    Ejercicio 5 y 6

---
+++ Tema02 - Introducción a la programación/Ejercicios/Bloque 2/Ejercicio6/Ejercicio6/Program.cs
@@ -0,0 +1,21 @@
+﻿//Declaramos las variables
+double numero1, numero2, suma, resta, multiplicacion;
+
+//Pido la info al usuario
+Console.Write("Introduce un número: ");
+numero1 = double.Parse(Console.ReadLine());
+
+Console.Write("Introduce otro número: ");
+numero2 = double.Parse(Console.ReadLine());
+
+//Realizo los cálculos
+suma = numero1 + numero2;
+resta = numero1 - numero2;
+multiplicacion = numero1 * numero2;
+
+//Mostramos los datos por pantalla
+Console.WriteLine("La suma es: " + suma);
+Console.WriteLine("La resta es: " + resta);
+Console.WriteLine("La multiplicación es: " + multiplicacion);
+
+Console.ReadLine();
---
+++ Tema02 - Introducción a la programación/Ejercicios/Bloque 2/Ejercicio8/Ejercicio8/Program.cs
@@ -0,0 +1,22 @@
+﻿//pv = nRT
+//Declaramos las variables
+double presion, volumen, nMoles, temperatura;
+const double R = 0.082;
+
+//Pedimos los datos al usuario
+Console.Write("Mete el volumen: ");
+volumen = double.Parse(Console.ReadLine());
+
+Console.Write("Mete el número de moles: ");
+nMoles = double.Parse(Console.ReadLine());
+
+Console.Write("Mete la temperatura: ");
+temperatura = double.Parse(Console.ReadLine());
+
+//Realizamos los cálculos
+presion = (nMoles * R * temperatura) / volumen;
+
+//Mostramos los datos por pantalla
+Console.WriteLine("Con un volumen de " + volumen + " litros ,y una temperatura de " + temperatura + " kelvin, " + nMoles + " moles de un gas ideal tienen una presión de " + presion + " atmósferas");
+
+Console.ReadLine();
